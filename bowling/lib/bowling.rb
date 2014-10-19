358f79f4bacbad5a5fc4b5b45f00ae3a97b5348f
Ryan <hiphubryan@gmail.com>
2014-10-19 22:11:02 +0800

    finish bowling kata

---
+++ bowling/lib/bowling.rb
@@ -0,0 +1,33 @@
+class BowlingGame
+
+  attr_reader :score
+
+  def initialize
+    @score = 0
+    @record = []
+  end
+
+  def roll_many(*n)
+    n.each do |i|
+      roll(i)
+    end
+  end
+
+  def roll(n)
+    @record << n
+
+    if n == "/"
+      @score += ( 10 - previous_ball )
+    else
+      @score += n
+    end
+  end
+
+  private
+
+  def previous_ball
+    @record[-2]
+  end
+
+
+end

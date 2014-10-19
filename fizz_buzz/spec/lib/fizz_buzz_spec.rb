7110f5949ebfff2788c268f2767fe9f2a2cc80bf
Ryan <hiphubryan@gmail.com>
2014-10-19 20:31:41 +0800

    Finish fizz_buzz

---
+++ fizz_buzz/spec/lib/fizz_buzz_spec.rb
@@ -0,0 +1,23 @@
+require_relative "../../lib/fizz_buzz"
+
+describe FizzBuzz do
+
+  let(:converter){ converter = FizzBuzz.new}
+
+  it "should convert 1 to 1" do 
+    expect( converter.convert(1) ).to eq 1
+  end
+
+  it "should convert 3 to Fizz" do
+    expect( converter.convert(3) ).to eq "Fizz"
+  end
+
+  specify "should convert 5 to buzz" do
+    expect( converter.convert(5) ).to eq "Buzz"
+  end
+
+  example "should convert 15 to FizzBuzz" do
+    expect( converter.convert(15) ).to eq "FizzBuzz"
+  end
+
+end

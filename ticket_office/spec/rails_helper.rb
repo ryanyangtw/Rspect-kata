b2f40c1cdd379d5a22b1ac09598aa8a9a61de565
Ryan <hiphubryan@gmail.com>
2014-10-20 22:13:15 +0800

    Add support

--- ticket_office/spec/rails_helper.rb
+++ ticket_office/spec/rails_helper.rb
@@ -18,7 +18,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
@@ -30,7 +30,9 @@ RSpec.configure do |config|
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
-  # instead of true.
+  # instead of true
+
+  config.include FactoryHelpers
   config.use_transactional_fixtures = true
 
   # RSpec Rails can automatically mix in different behaviours to your tests

0eba39f56708a349f97891b37b30ae581cfc18a7
Ryan <hiphubryan@gmail.com>
2014-10-20 15:18:47 +0800

    Add train model and validation spec

---
+++ ticket_office/spec/models/train_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Train, :type => :model do
+
+
+  it "should not be valid if balnk name" do 
+    expect(Train.new).to_not be_valid
+  end
+
+  it "should be valid if it has name" do 
+    train = Train.new(:name => "Q_Q")
+    expect(train).to be_valid
+  end
+
+
+
+end

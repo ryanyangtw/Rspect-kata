4dc51745cda2ee9062b9a140b0080048c73cd585
Ryan <hiphubryan@gmail.com>
2014-10-20 20:30:23 +0800

    User real train

--- ticket_office/spec/controllers/trains_controller_spec.rb
+++ ticket_office/spec/controllers/trains_controller_spec.rb
@@ -3,20 +3,24 @@ require 'rails_helper'
 RSpec.describe TrainsController, :type => :controller do
 
   describe "get show" do
-    before do
-      Train = double("Train")
-      @train = double("train", :id=>"416", :name=>"Train#416")
-      allow(Train).to receive(:find).with(@train.id).and_return(@train)
-    end
+    
+    #原本是用stub的方式假造
+    #before do
+      #Train = double("Train")
+      #@train = double("train", :id=>"416", :name=>"Train#416")
+      #allow(Train).to receive(:find).with(@train.id).and_return(@train)
+    #end
 
+    #現在改用真的，資料真的會儘db
+    let(:train){ Train.create!(:name => "416") }
     it "should return train's seat data" do
 
-      get :show, {:id => 416, :format => :json }
-      expect(assigns(:train)).to eq(@train)
+      get :show, {:id => train.id, :format => :json }
+      expect(assigns(:train)).to eq(train)
       #要測template 必須把 template (views/show.json.jbuilder)確實實作出來
       expect(response).to render_template(:show)
       expect(response).to have_http_status(200)
-      
+
     end
 
   end

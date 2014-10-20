01847b080a942123b9996da245f524537a1ef709
Ryan <hiphubryan@gmail.com>
2014-10-20 20:54:06 +0800

    Finish Train#reserve

--- ticket_office/app/models/train.rb
+++ ticket_office/app/models/train.rb
@@ -7,10 +7,22 @@ class Train < ActiveRecord::Base
   def seats_by_date
 
    self.seats.map do |s|
+      #這邊應該是Ihower寫錯了
       #r = s.seat_reservations.last  
       r = s.reservations.last
       {"id" => s.name, "reservation_id" => r.try(:id)}
+    end
+  end
 
+
+  def reserve(email, seat)
+    r = Reservation.create!(:email => email)
+    s = self.seats.find_by_name(seat)
+
+    if s
+      s.seat_reservations.create!( :reservation => r) 
+    else
+      raise ActiveRecord::RecordNotFound
     end
   end
 
